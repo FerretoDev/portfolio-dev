7d30feb39d4b551dda74bcde5ea9ec336e863d81
Marcos Ferreto Estrada <marcosferretoestrada@gmail.com>
2025-02-03 20:53:03 -0600

    Actualizar estilos y estructura de la sección hero: ajustar tamaños, colores y mejorar la consistencia visual.

--- portfolio/components/sections/hero_section.py
+++ portfolio/components/sections/hero_section.py
@@ -11,11 +11,12 @@ class HeroStyle:
     hero_title: dict[str, str] = field(
         default_factory=lambda: {
             "size": "9",
-            "font_size": "2.5em",
             "background": rx.color_mode_cond(
-                light=f"linear-gradient(to right, {GlobalThemeVariables.LIGHT.value["--primary"]}, {GlobalThemeVariables.LIGHT.value["--secondary"]})",
-                dark=f"linear-gradient(to right, {GlobalThemeVariables.DARK.value["--primary"]}, {GlobalThemeVariables.DARK.value["--secondary"]})",
+                light=f"linear-gradient(to right, {GlobalThemeVariables.LIGHT.value['--primary']}, {GlobalThemeVariables.LIGHT.value['--secondary']})",
+                dark=f"linear-gradient(to right, {GlobalThemeVariables.DARK.value['--primary']}, {GlobalThemeVariables.DARK.value['--secondary']})",
             ),
+            "font_size": "3rem",
+            "background_image": "linear-gradient(to right, #06B6D4, #3B82F6)",
             "background_clip": "text",
             "color": "transparent",
         },
@@ -23,7 +24,8 @@ class HeroStyle:
 
     hero_image: dict[str, str] = field(
         default_factory=lambda: {
-            "border-radius": "10%",
+            # "border-radius": "10%",
+            "border-radius": "1em",
             "box-shadow": rx.color_mode_cond(
                 light="0 0 30px rgba(8, 145, 178, 0.3)",
                 dark="0 0 30px rgba(6, 182, 212, 0.3)",
@@ -31,6 +33,8 @@ class HeroStyle:
             "width": "10%",
             # "max_width": "100%",
             "height": "auto",
+            "size": "2",
+            "border": f"1px solid {GlobalThemeVariables.LIGHT.value['--primary']}",
         },
     )
     hero_text: dict[str, str] = field(
@@ -47,122 +51,42 @@ class HeroStyle:
 HeroStyle: HeroStyle = HeroStyle()
 
 
-def hero_section2() -> rx.Component:
-    """Hero section of the application"""
-    return rx.box(
-        rx.flex(
-            rx.vstack(
-                rx.heading(
-                    "Software Development + Mathematical Modeling",
-                    **HeroStyle.hero_title,
-                ),
-                rx.text(
-                    "Transformando datos y algoritmos en soluciones innovadoras.",
-                    color="whiteAlpha.800",
-                    font_size="1.2em",
-                ),
-                rx.hstack(
-                    rx.button(
-                        "Ver Proyectos",
-                        bg="cyan.500",
-                        _hover={"bg": "cyan.600"},
-                    ),
-                    rx.button(
-                        "Contactar",
-                        variant="outline",
-                        border_color="cyan.500",
-                        color="cyan.500",
-                        _hover={"bg": "rgba(6, 182, 212, 0.1)"},
-                    ),
-                    padding_top="1em",
-                ),
-                align_items="start",
-                spacing="1",
-                width="100%",
-            ),
-            rx.image(
-                src="/Designer.jpeg",
-                border_radius="1em",
-                width="10%",
-                box_shadow="0 0 30px rgba(6, 182, 212, 0.3)",
-            ),
-            max_width="1200px",
-            margin="0 auto",
-            padding_top="8em",
-            padding="2em",
-            align_items="center",
-            justify_content="space-between",
-        )
-    )
-
-
-def hero_section1_3() -> rx.Component:
-    return rx.box(
-        rx.vstack(
-            rx.heading(
-                "Software Development + Mathematical Modeling",
-                **HeroStyle.hero_title,
-            ),
-            rx.text(
-                "Transformando datos y algoritmos en soluciones innovadoras.",
-                **HeroStyle.hero_text,
-            ),
-            rx.hstack(
-                rx.button(
-                    "Ver Proyectos",
-                    size="2",
-                    color_scheme="cyan",
-                ),
-                rx.button(
-                    "Contactar",
-                    size="2",
-                    variant="outline",
-                    color_scheme="cyan",
-                ),
-                rx.spacer(),
-                rx.image(
-                    src="/Designer.jpeg",
-                    **HeroStyle.hero_image,
-                ),
-                padding_top="1em",
-                align="center",
-            ),
-            align="center",
-            spacing="6",
-            padding_y="20",
-        ),
-        max_width="1200px",
-        margin="0 auto",
-        padding_top="8em",
-        padding_x="2em",
-        align_items="center",
-        justify_content="space-between",
-    )
-
-
-def hero_section():
+def hero_section() -> rx.Component:
     """Sección principal del hero."""
     return rx.box(
         rx.hstack(
             rx.vstack(
-                rx.text("Desarrollador de Software", color="#06B6D4"),
+                rx.text(
+                    "Desarrollador de Software",
+                    color=rx.color_mode_cond(
+                        light=GlobalThemeVariables.LIGHT.value["--primary"],
+                        dark=GlobalThemeVariables.DARK.value["--primary"],
+                    ),
+                    size="7",
+                    # **HeroStyle.hero_text,
+                    # color="#06B6D4",
+                ),
                 rx.heading(
                     "Transformando Ideas en Soluciones Tecnológicas",
-                    font_size="3rem",
-                    background_image="linear-gradient(to right, #06B6D4, #3B82F6)",
-                    background_clip="text",
-                    color="transparent",
+                    **HeroStyle.hero_title,
                 ),
                 rx.text(
                     "Especializado en desarrollo de software, modelación matemática y análisis de datos. "
                     "Creando soluciones innovadoras con un enfoque analítico y técnico.",
-                    color="#D1D5DB",
+                    # color="#D1D5DB",
+                    color=rx.color_mode_cond(
+                        light=GlobalThemeVariables.LIGHT.value["--secondary"],
+                        dark=GlobalThemeVariables.DARK.value["--secondary"],
+                    ),
                     margin_y="1rem",
                 ),
                 rx.hstack(
                     rx.button(
                         "Ver Proyectos",
-                        background_color="#06B6D4",
+                        background_color=rx.color_mode_cond(
+                            light=GlobalThemeVariables.LIGHT.value["--primary"],
+                            dark=GlobalThemeVariables.DARK.value["--primary"],
+                        ),
                         color="white",
                         padding_x="1.5rem",
                         padding_y="0.75rem",
@@ -171,15 +95,21 @@ def hero_section():
                     rx.button(
                         "Descargar CV",
                         background_color="transparent",
-                        border="1px solid #06B6D4",
-                        color="#06B6D4",
+                        border=f"1px solid {GlobalThemeVariables.LIGHT.value['--primary']}",
+                        color=rx.color_mode_cond(
+                            light=GlobalThemeVariables.LIGHT.value["--primary"],
+                            dark=GlobalThemeVariables.DARK.value["--primary"],
+                        ),
                         padding_x="1.5rem",
                         padding_y="0.75rem",
                     ),
                 ),
                 width="50%",
             ),
-            rx.avatar(src="Designer.jpeg", size="2", border="4px solid #06B6D4"),
+            rx.avatar(
+                src="Designer.jpeg",
+                **HeroStyle.hero_image,
+            ),
             width="100%",
             align_items="center",
             justify_content="space-between",
