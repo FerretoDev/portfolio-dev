e70fbedaae18d103fa63cf8e07eea2f14395cb17
Marcos Ferreto Estrada <marcosferretoestrada@gmail.com>
2025-06-30 13:18:26 -0600

    Reorganizar importaciones en card.py y eliminar función 'suma'

--- portfolio/components/card.py
+++ portfolio/components/card.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass, field
+from typing import List, Optional
 
 import reflex as rx
+
 from portfolio.components.styles.styles import CardStyle
-from typing import Optional, List
 
 
 def card(title: str, description: str, tag: str):
@@ -19,26 +20,3 @@ def card(title: str, description: str, tag: str):
         rx.icon(tag, **CardStyle.icon),
         **CardStyle.base,
     )
-
-
-def suma(
-    title: str,
-    description: str,
-    tag: str,
-    icon: str,
-    background: str,
-):
-    return rx.hstack(
-        rx.vstack(
-            rx.text(title, **CardStyle.title),
-            rx.text(
-                description,
-                **CardStyle.description,
-            ),
-            **CardStyle.stack,
-        ),
-        rx.box(background, **CardStyle.background),
-        rx.icon(icon, **CardStyle.icon),
-        **CardStyle.base,
-    )
-    
